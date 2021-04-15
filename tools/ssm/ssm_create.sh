6f4585bc0fe06b1a5e7850860e205140ed6749b3
Vastin He <heayh@amazon.com>
2021-04-15 16:53:30 -0500

    CD test

--- tools/ssm/ssm_create.sh
+++ tools/ssm/ssm_create.sh
@@ -17,31 +17,37 @@ set -eu
 PKG_NAME=$1
 REL_VERSION=$2
 S3_BUCKET=$3
+REGION=$4
 
-output=$(aws ssm list-documents --filter Key=Owner,Values=Self Key=DocumentType,Values=Package Key=Name,Values=${PKG_NAME})
+output=$(aws --region ${REGION} ssm list-documents --filter Key=Owner,Values=Self Key=DocumentType,Values=Package Key=Name,Values=${PKG_NAME})
 echo ${output}
 status=$(echo ${output} | python3 -c "import sys, json; print('empty') if len(json.load(sys.stdin)['DocumentIdentifiers']) == 0 else print('exist')")
 if [[ ${status} == "empty" ]]
 then
-    aws ssm create-document \
+    aws --region ${REGION} ssm create-document \
         --name "${PKG_NAME}" \
         --content file://build/packages/ssm/manifest.json \
         --attachments Key="SourceUrl",Values="https://s3.amazonaws.com/${S3_BUCKET}" \
         --version-name ${REL_VERSION} \
         --document-type Package
 else
-    output=$(aws ssm update-document \
-        --name "${PKG_NAME}" \
-        --content file://build/packages/ssm/manifest.json \
-        --attachments Key="SourceUrl",Values="https://s3.amazonaws.com/${S3_BUCKET}" \
-        --version-name ${REL_VERSION} \
-        --document-version "\$LATEST")
+    len=$(aws --region ${REGION} ssm list-document-versions --name "${PKG_NAME}" | python3 -c "import sys, json; print(len([ver for ver in json.load(sys.stdin)['DocumentVersions'] if ver['VersionName'] == '${REL_VERSION}']))")
+    if [[ "$len" -eq 0 ]]; then
+        output=$(aws --region ${REGION} ssm update-document \
+            --name "${PKG_NAME}" \
+            --content file://build/packages/ssm/manifest.json \
+            --attachments Key="SourceUrl",Values="https://s3.amazonaws.com/${S3_BUCKET}" \
+            --version-name ${REL_VERSION} \
+            --document-version "\$LATEST")
 
-    echo ${output}
-    last_version=$(echo ${output} | python3 -c "import sys, json; print(json.load(sys.stdin)['DocumentDescription']['LatestVersion'])")
-    echo ${last_version}
+        echo ${output}
+        last_version=$(echo ${output} | python3 -c "import sys, json; print(json.load(sys.stdin)['DocumentDescription']['LatestVersion'])")
+        echo ${last_version}
 
-    aws ssm update-document-default-version \
-        --name "${PKG_NAME}" \
-        --document-version "${last_version}"
+        aws --region ${REGION} ssm update-document-default-version \
+            --name "${PKG_NAME}" \
+            --document-version "${last_version}"
+    else
+        echo "${PKG_NAME} ${REL_VERSION} exists in ${REGION}."
+    fi
 fi
