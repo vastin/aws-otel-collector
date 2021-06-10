e388f73d4f037a0ca8b97974ace050ef8fed337f
Vastin He <heayh@amazon.com>
2021-06-09 23:13:32 -0500

    auto re-run

---
+++ tools/workflow/rerun_workflow.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import argparse
+import time
+import os
+from ghapi.all import GhApi
+
+timeout = 3*60*60 # wait for max 3 hours
+interval = 5*60 # wait for 5 minutes between query status
+
+parser = argparse.ArgumentParser(description="Re-run failed GitHub workflow")
+parser.add_argument("owner", type=str, help="owner")
+parser.add_argument("repo", type=str, help="repository")
+parser.add_argument("run", type=str, help="run id")
+args = parser.parse_args()
+
+api = GhApi(args.owner, args.repo)
+run_id = args.run
+
+start = int(time.time())
+while (int(time.time()) - start < timeout):
+    status = api.actions.get_workflow_run(run_id).status
+    print("Workflow Status:", status)
+    if status == "completed":
+        result = api.actions.get_workflow_run(run_id).conclusion
+        print("Workflow Result:", result)
+        if result != "success" and result != "cancelled":
+            api.actions.re_run_workflow(run_id)
+            print("Re-run workflow")
+            os.exit(0)
+    else:
+        time.sleep(interval)
+        print("Sleep %d seconds" % interval)
+print("Time out!!!")
+
+
